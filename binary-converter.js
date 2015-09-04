21fdc5f3ab7e46c07c2f9da909ca9d1546bf7692
WhimsicalWill <knipewilliam20@gmail>
2015-09-04 17:24:48 -0400

    Made it Work

--- binary-converter.js
+++ binary-converter.js
@@ -1,14 +1,69 @@
 function compute() {
-    if (document.getElementById("binary").value.length > 0) {
-        var input = document.getElementById("binary").value;  
-        document.getElementById("decimal").placeholder = BinaryToDecimal(input);
-    }
-    else if (document.getElementById("decimal").value.length > 0) {
-        var input = document.getElementById("decimal").value;  
-        document.getElementById("binary").placeholder = DecimalToBinary(input, "");
-    }
+        var inputNumber = document.getElementById("input").value;  
+    
+        var inputBaseOptions = document.getElementById("inputBaseOptions");
+        var inputBase = parseInt(inputBaseOptions.options[inputBaseOptions.selectedIndex].value);
+    
+        console.log("Input Base:" + typeof inputBase);
+        
+        var outputBaseOptions = document.getElementById("outputBaseOptions");
+        var outputBase = parseInt(outputBaseOptions.options[outputBaseOptions.selectedIndex].value);
+    
+        console.log("Output Base:" + outputBase);
+        console.log("Input Number:" + inputNumber);
+    
+        document.getElementById("output").value = BaseAtoBaseB(inputBase, outputBase, inputNumber); 
 }
 
+function BaseAtoBaseB(a, b, num) {
+    console.log(a + " " + b);
+    if (a > b) {
+        
+        var decNum = BaseAtoBaseB(a, 10, num);
+        console.log("a > b");
+        return ConvertGreaterBase(10, b, decNum, "");
+    }
+    else if (a < b){
+        console.log("a < b");
+        var decNum = ConvertLowerBase(num.length - 1, num.toString(), a);
+        return ConvertGreaterBase(10, b, decNum, "");
+    }
+    
+    else {
+        console.log("a = b");
+      return num;   
+    }
+    
+}
+
+function ConvertLowerBase(exponent, number, base) {
+        if (exponent == -1) {
+            console.log("CLB " + 0);
+            return 0; 
+        }
+        else {
+            console.log("CLB " + number[0] * Math.pow(base, exponent));
+            return ConvertLowerBase(exponent - 1, number.substring(1), base) + number[0] *  Math.pow(base, exponent);
+        }
+}
+
+function ConvertGreaterBase(a, b, num, result) {
+    //Base case
+    if (num == 0) {
+        console.log("CRB " + result);
+        return result;
+    }
+    
+    var number = Math.floor(num / b);
+    var remainder = num % b;
+    
+    result = remainder + result;
+    console.log("CRB " + result);
+        
+    return ConvertGreaterBase(a, b, number, result);
+}
+
+//binary is the binary number to be converted to decimal
 function BinaryToDecimal(binary) {
     var size = binary.length;
     var binaryString = binary.toString();
@@ -20,6 +75,8 @@ function BinaryToDecimal(binary) {
     return decimal;
 }
 
+//decimal is the decimal number to be converted to binary.
+//binary is a string storing the binary number.
 function DecimalToBinary(decimal, binary) {
     //Base case
     if (decimal == 0) {
